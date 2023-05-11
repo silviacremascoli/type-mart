f688b9713c2af92f7f4538f54f26619059d73f72
silviacremascoli <silcremascoli@gmail.com>
2023-05-11 16:23:19 +0200

    Added variables and conditionals

--- index.ts
+++ index.ts
@@ -1,7 +1,33 @@
 import products from "./products";
 
-let productName: string = "beanie";
+let productName: string = "fanny pack";
+let shipping: number;
+let taxPercent: number;
+let taxTotal: number;
+let total: number;
+let shippingAddress: string = "592 Five Points, 20151 Maryland";
 
-let product = products.filter((product) => {
-  return product.name === productName;
-})[1];
+let product = products.filter((product: { name: string; }) => {
+  return product.name == productName;
+})[0];
+
+function preOrder() {
+  if (product.preOrder === true) {
+    console.log(`This product can be pre-ordered. We will notify you when it's on the way`)
+  } else {
+    null
+  }
+}
+
+function freeShipping() {
+if (product.price >= 25) {
+  shipping = 0;
+  console.log(`Hurray! You get free shipping for this product.`)
+} else {
+  shipping = 5;
+}
+}
+
+console.log(product);
+preOrder();
+freeShipping();
