ebc940a6e6363553aab9fbdb1af626e77596b64a
silviacremascoli <silcremascoli@gmail.com>
2023-05-11 16:26:35 +0200

    Corrected functions and products types

--- index.ts
+++ index.ts
@@ -7,27 +7,29 @@ let taxTotal: number;
 let total: number;
 let shippingAddress: string = "592 Five Points, 20151 Maryland";
 
-let product = products.filter((product: { name: string; }) => {
-  return product.name == productName;
+let product = products.filter((product) => {
+  return product.name === productName;
 })[0];
 
-function preOrder() {
+function preOrder(product) {
   if (product.preOrder === true) {
-    console.log(`This product can be pre-ordered. We will notify you when it's on the way`)
+    console.log(
+      `This product can be pre-ordered. We will notify you when it's on the way`
+    );
   } else {
-    null
+    null;
   }
 }
 
-function freeShipping() {
-if (product.price >= 25) {
-  shipping = 0;
-  console.log(`Hurray! You get free shipping for this product.`)
-} else {
-  shipping = 5;
-}
+function freeShipping(product) {
+  if (product.price >= 25) {
+    shipping = 0;
+    console.log(`Hurray! You get free shipping for this product.`);
+  } else {
+    shipping = 5;
+  }
 }
 
 console.log(product);
-preOrder();
-freeShipping();
+preOrder(product);
+freeShipping(product);
