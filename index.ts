a24a82641e227adff676ec3cf7c4a7036fd2e22c
silviacremascoli <silcremascoli@gmail.com>
2023-05-12 11:02:00 +0200

    Fixed functions and added final console log

--- index.ts
+++ index.ts
@@ -7,38 +7,37 @@ let taxTotal: number;
 let total: number;
 let shippingAddress: string = "3763 Farnum Road, 10016 New York";
 
-let product = products.filter((product) => {
+const product = products.filter((product) => {
   return product.name === productName;
 })[0];
 
-function preOrder(product) {
-  if (product.preOrder === true) {
-    console.log(
-      `This product can be pre-ordered. We will notify you when it's on the way.`
-    );
-  } else {
-    null;
-  }
+if (product.preOrder) {
+  console.log(
+    `This product can be pre-ordered. We will notify you when it's on the way.`
+  );
 }
 
-function freeShipping(product) {
-  if (product.price >= 25) {
-    shipping = 0;
-    console.log(`Hurray! You get free shipping for this product.`);
-  } else {
-    shipping = 5;
-  }
+if (product.price >= 25) {
+  shipping = 0;
+  console.log(`Hurray! You get free shipping for this product.`);
+} else {
+  shipping = 5;
 }
 
-function taxNY(shippingAddress: string) {
-  if (shippingAddress.match("New York")) {
-    taxPercent = 0.1;
-  } else {
-    taxPercent = 0.05;
-  }
+if (shippingAddress.match("New York")) {
+  taxPercent = 0.1;
+} else {
+  taxPercent = 0.05;
 }
 
-console.log(product);
-preOrder(product);
-freeShipping(product);
-taxNY(shippingAddress);
+taxTotal = product.price * taxPercent;
+total = product.price + taxTotal + shipping;
+
+console.log(`
+Product name: ${productName}
+Address: ${shippingAddress}
+Price: $${product.price}
+Tax total: $${taxTotal.toFixed(2)}
+Shipping: $${shipping.toFixed(2)}
+Total: $${total.toFixed(2)}
+`);
