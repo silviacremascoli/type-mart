a2c5621348b46c91959c59da45646f1d5b1f715c
silviacremascoli <silcremascoli@gmail.com>
2023-05-12 11:12:46 +0200

    Added tsconfig.json and compiled

---
+++ index.js
@@ -0,0 +1,38 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const products_1 = require("./products");
+let productName = "shirt";
+let shipping;
+let taxPercent;
+let taxTotal;
+let total;
+let shippingAddress = "3763 Farnum Road, 10016 New York";
+const product = products_1.default.filter((product) => {
+    return product.name === productName;
+})[0];
+if (product.preOrder) {
+    console.log(`This product can be pre-ordered. We will notify you when it's on the way.`);
+}
+if (product.price >= 25) {
+    shipping = 0;
+    console.log(`Hurray! You get free shipping for this product.`);
+}
+else {
+    shipping = 5;
+}
+if (shippingAddress.match("New York")) {
+    taxPercent = 0.1;
+}
+else {
+    taxPercent = 0.05;
+}
+taxTotal = product.price * taxPercent;
+total = product.price + taxTotal + shipping;
+console.log(`
+Product name: ${productName}
+Address: ${shippingAddress}
+Price: $${product.price}
+Tax total: $${taxTotal.toFixed(2)}
+Shipping: $${shipping.toFixed(2)}
+Total: $${total.toFixed(2)}
+`);
